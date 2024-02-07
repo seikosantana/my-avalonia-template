0fd38127334b1ecc2d5c0fade02469786941a97c
Seiko Santana <santanaaseiko@gmail.com>
2024-02-07 15:41:12 +0700

    Add splash window

---
+++ MyAvaloniaTemplate/Views/SplashWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MyAvaloniaTemplate.Views;
+
+public partial class SplashWindow : Window
+{
+    public SplashWindow()
+    {
+        InitializeComponent();
+    }
+}
