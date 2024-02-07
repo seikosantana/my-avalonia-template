868b5f7e72f4051b49f2a7e4f4df622bf9f98d29
Seiko Santana <santanaaseiko@gmail.com>
2024-02-07 15:36:39 +0700

    Add AppWindow

---
+++ MyAvaloniaTemplate/Views/AppWindow.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Platform;
+
+namespace MyAvaloniaTemplate.Views;
+
+public class AppWindow : Window
+{
+    public AppWindow() : base()
+    {
+        Activated += (_, _) =>
+        {
+            if (Application.Current is App app)
+            {
+                app.ActiveWindow = this;
+            }
+        };
+    }
+
+    public AppWindow(IWindowImpl impl) : base(impl)
+    {
+    }
+}
